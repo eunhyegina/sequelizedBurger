40fb4303a032cceaac284e825979345b0c6e9f90
eunhyegina <eunhyegina@gmail.com>
2019-04-26 16:41:42 -0700

    v1

---
+++ models/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", True);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Jalapeno Burger", True);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hamburger", False);
---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+// var db = require("../models");
+
+// module.exports = function(app) {
+//   app.post("/api/burger", function(req, res) {
+//     db.Burger.create(req.body).then(function(cBurger) {
+//       res.json(cBurger);
+//     });
+//   });
+
